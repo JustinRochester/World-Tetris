de69d7eb4a813b5b95e73e6f2678b27534ed1b55
Tajx <1135854336@qq.com>
2020-05-20 00:20:31 +0800

    modify Brick.h

--- Brick/Brick.h
+++ Brick/Brick.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstdlib>
-#include "constnum.h"
 
 class Brick {
 private:
@@ -14,7 +13,9 @@ private:
 	void shiftUp(int pace = 1);//上移
 	void brickUpdate();//更新颜色和类型
 	void rotateBrick();//顺时针旋转
+	static const int TotalBrickType = 17, TotalColorType = 4;
 public:
+	static const int Up = 1, Down = 2,Left = 3, Right = 4, Rotate = 5, Update = 6;
 	Brick();//初始化类型和颜色
 	void brickSet(int x, int y);//通过x,y初始化坐标
 	void Operation(int _Order);//识别指令
