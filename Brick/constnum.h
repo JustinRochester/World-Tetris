305efd1eae560bdc4a4626bfdbbd3449b4180d60
Tajx <1135854336@qq.com>
2020-05-19 23:56:19 +0800

    add new file - constnum.h

---
+++ Brick/constnum.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#define TotalColorType 4//颜色总数
+#define TotalBrickType 17	//类型总数
+#define Up 1//上移指令
+#define Down 2//下移指令
+#define Left 3//左移指令
+#define Right 4//右移指令
+#define Rotate 5//旋转指令
+#define Update 6//更新颜色及类型指令
