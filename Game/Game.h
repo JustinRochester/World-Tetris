c40ef9336ecbf540f08c1839a4d71443a246216b
JustinRochester <60056121+JustinRochester@users.noreply.github.com>
2020-05-28 17:13:02 +0800

    update Game.h

--- Game/Game.h
+++ Game/Game.h
@@ -1,13 +1,16 @@
+#include"Player.h"
+#include"Render.h"
 class Game {
 private:
 	Player player[2];
 	int Gamemode;
 	static int NumberOfFrames;
+	bool gameShouldOver();
+	bool IsUsedCommand();
+	void setGameMode();
+	void play();
 
 public:	
 	Game();	
-	bool gameShouldOver();
 	void run();
-	
-	void setGameMode();
 }; 
