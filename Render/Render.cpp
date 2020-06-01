68dd0448257fa579a49cf25fb7638c51a7479d1d
Tajx <1135854336@qq.com>
2020-06-01 20:55:40 +0800

    update Render.cpp

--- Render/Render.cpp
+++ Render/Render.cpp
@@ -1,13 +1,9 @@
-#include<cstdlib>
-#include<windows.h>
-#include<conio.h>
-#ifndef _Render
-#define _Render#include"Render.h"
+#include"Render.h"
 #include<iostream>
 using namespace std;
 
 void Render::SetTitle() {
-	string Title = "这是我罗斯方块哒^^";
+	string Title = "我罗斯方块哒";
 	SetConsoleTitle(Title.c_str());
 }
 void Render::HideCursor() {
@@ -387,20 +383,4 @@ void Render::DrawMap2(int score1, int score2)				//双人画游戏时界面
 
 } 
 
-
-class Render
-{
-private:
-	int type;				//模式 
-public:
-	void Welcome();			//首界面
-	void SetColor(int);		//控制颜色
-	void DrawMap1(int score);			//游戏界面
-	void DrawMap2(int score1, int score2);
-	void DrawGame1(int Map[32][12], int BrickCol, const int *NextBrick);  //single-player 
-	static void HideCursor();
-	static void SetTitle();
-};
-
-#endif
 
